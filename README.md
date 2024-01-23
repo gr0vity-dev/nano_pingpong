d36613810eade9fb7d608d71ddf8451ef87643d8
gr0vity-dev <gr0vity-dev@mail.void>
2024-01-23 13:43:58 +0100

    remove dependency from local nano_node and use rpc.nano.to instead

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 
 ## Requirements :
-- Access to a nano_node with work_generation enabled
+- free AUTH_KEY from rpc.nano.to
 - python and the dependencies from requirements.txt
 
 ## Run the script
@@ -10,5 +10,6 @@
 git clone https://github.com/gr0vity-dev/nano_pingpong.git
 cd nano_pingpong
 pip install -r requirements.txt
+// add you AUTH_KEY to config.py 
 python3 nano_refundbot.py
 ```
--- nano_refundbot.py
+++ nano_refundbot.py
@@ -1,110 +1,41 @@
 import asyncio
-from nano_lib_py import Block, generate_account_id, get_account_public_key, generate_account_private_key, generate_seed
-from nanorpc.client import NanoRpc, NodeVersion
+from nanotopy.client import NanoTo
+from config import AUTH_KEY
 
 
 class NanoPingPongBot:
-    def __init__(self, rpc_url, node_version, rpc_user=None, rpc_password=None):
-        self.rpc = NanoRpc(url=rpc_url, username=rpc_user,
-                           password=rpc_password, node_version=node_version)
-        self.seed = generate_seed()
-        self.private_key = generate_account_private_key(self.seed, 0)
-        self.bot_account_address = generate_account_id(self.seed, 0)
+    def __init__(self, auth_key):
+        self.nano_to = NanoTo(auth_key=auth_key)
+        self.seed = NanoTo.generate_seed()
+        self.private_key = NanoTo.get_private_key_from_seed(self.seed, 0)
+        self.bot_account_address = NanoTo.get_account_from_key(
+            self.private_key)
 
         print(f"Generated Seed: {self.seed}")
         print(f"Your Address: {self.bot_account_address}")
         print("Visit https://nanodrop.io/ to get some nano.")
 
-    async def check_for_incoming_transactions(self, count="5", source="true"):
-        return await self.rpc.receivable(self.bot_account_address, count=count, source=source)
-
-    async def get_account_frontier(self):
-        response = await self.rpc.frontiers(self.bot_account_address, 1)
-        return response.get("frontiers", {}).get(self.bot_account_address, "0" * 64)
-
-    async def get_pow_hash(self, account_frontier):
-        return get_account_public_key(account_id=self.bot_account_address) if account_frontier == "0" * 64 else account_frontier
-
-    def get_link_as_hash(self, link):
-        link = get_account_public_key(account_id=link) if str(
-            link).startswith("nano_") else link
-        return link
-
-    async def get_pow(self, frontier):
-        pow_response = await self.rpc.work_generate(await self.get_pow_hash(frontier), use_peers="true")
-        return pow_response["work"]
-
-    async def create_send(self, frontier, current_balance, amount_to_send, destination):
-        final_balance = current_balance - amount_to_send
-        return await self.create_block(frontier, final_balance, destination)
-
-    async def create_receive(self, frontier, incoming_amount, send_hash):
-        return await self.create_block(frontier, incoming_amount, send_hash)
-
-    async def create_block(self, frontier, balance, link):
-
-        block = Block(
-            block_type="state",
-            account=self.bot_account_address,
-            representative=self.bot_account_address,
-            previous=frontier,
-            balance=balance,
-            link=self.get_link_as_hash(link),
-            work=await self.get_pow(frontier)
-        )
-        block.sign(self.private_key)
-        return block
-
-    async def return_entire_balance_to_latest_sender(self):
-        account_info = await self.rpc.account_info(self.bot_account_address)
-        account_balance = int(account_info.get("balance", "0"))
-
-        if account_balance > 0:
-            account_frontier_hash = await self.get_account_frontier()
-            frontier_info = await self.rpc.blocks_info([account_frontier_hash], source=True)
-            received_from = frontier_info.get("blocks", {}).get(
-                account_frontier_hash, {})["source_account"]
-            amount_to_send = account_balance
-
-            print(f"Returning funds to account: {received_from}")
-
-            send_block = await self.create_send(
-                account_frontier_hash, account_balance, amount_to_send, received_from)
-            send_hash = await self.rpc.process(send_block.json(), json_block=False)
-            print(f"Send block was created successfully: {send_hash}")
-            return True
-
-        return False
-
-    async def receive_single_incoming_transaction(self):
-        incoming_transactions = await self.check_for_incoming_transactions()
-
-        if incoming_transactions.get("blocks", ""):
-            first_block_hash, first_block_info = next(
-                iter(incoming_transactions["blocks"].items()))
-
-            print(f"Process incoming tx: {first_block_hash}")
-            current_frontier = await self.get_account_frontier()
-            receive_amount = int(first_block_info['amount'])
-            receive_block = await self.create_receive(
-                current_frontier, receive_amount, first_block_hash)
-
-            receive_hash = await self.rpc.process(receive_block.json(), json_block="false")
-            print(f"Receive block was created successfully: {receive_hash}")
-        else:
-            pass
-
     async def run(self):
         while True:
-            await self.receive_single_incoming_transaction()
-            await self.return_entire_balance_to_latest_sender()
-            await asyncio.sleep(1)
+            receivable_blocks = await self.nano_to.receivable(self.bot_account_address, source=True, threshold=10**24)
+            for block in receivable_blocks:
+                block_hash = block["hash"]
+                amount = block["amount"]
+                source_account = block["source"]
+
+                print(f"Process incoming tx: {block_hash} for {amount} raw")
+                print(receivable_blocks)
+                receive_hash = await self.nano_to.receive_block(self.private_key, block["amount"], block_hash)
+                print(f"Receive block created: {receive_hash}")
+
+                print(f"Returning funds to account: {source_account}")
+                refund_hash = await self.nano_to.send(self.private_key, amount, source_account)
+                print(f"Send block was created successfully: {refund_hash}")
+
+            await asyncio.sleep(10)
 
 
 # Main execution
 if __name__ == "__main__":
-    bot = NanoPingPongBot(rpc_url='http://localhost:7076',
-                          rpc_user=None,
-                          rpc_password=None,
-                          node_version=NodeVersion.V25_0)
+    bot = NanoPingPongBot(auth_key=AUTH_KEY)
     asyncio.run(bot.run())
