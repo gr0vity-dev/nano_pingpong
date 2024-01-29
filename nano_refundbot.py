3bb0cce8f29196f710f1a44726a2b5a522415e6e
gr0vity-dev <gr0vity-dev@mail.void>
2024-01-29 22:48:22 +0100

    move to websockets for confirmations

--- nano_refundbot.py
+++ nano_refundbot.py
@@ -1,10 +1,12 @@
 import asyncio
 from nanotopy.client import NanoTo
+from nanows.api import NanoWebSocket
 from config import AUTH_KEY
 
 
 class NanoPingPongBot:
     def __init__(self, auth_key):
+        self.nano_ws = NanoWebSocket(url="wss://nanowallet.cc/ws")
         self.nano_to = NanoTo(auth_key=auth_key)
         self.seed = NanoTo.generate_seed()
         self.private_key = NanoTo.get_private_key_from_seed(self.seed, 0)
@@ -16,21 +18,27 @@ class NanoPingPongBot:
         print("Visit https://nanodrop.io/ to get some nano.")
 
     async def run(self):
+        await self.nano_ws.subscribe_confirmation(self.bot_account_address)
+
         while True:
-            receivable_blocks = await self.nano_to.receivable(self.bot_account_address, source=True, threshold=10**24)
-            for block in receivable_blocks:
-                block_hash = block["hash"]
-                amount = block["amount"]
-                source_account = block["source"]
+            # Wait for new confirmation
+            async for confirmation in self.nano_ws.get_confirmations():
+                confirmation_message = confirmation.get("message", {})
+                block_type = confirmation_message.get(
+                    "block", {}).get("subtype")
 
-                print(f"Process incoming tx: {block_hash} for {amount} raw")
-                print(receivable_blocks)
-                receive_hash = await self.nano_to.receive_block(self.private_key, block["amount"], block_hash)
-                print(f"Receive block created: {receive_hash}")
+                if block_type == "send":
+                    block_hash = confirmation_message["hash"]
+                    amount = confirmation_message["amount"]
+                    source_account = confirmation_message["account"]
 
-                print(f"Returning funds to account: {source_account}")
-                refund_hash = await self.nano_to.send(self.private_key, amount, source_account)
-                print(f"Send block was created successfully: {refund_hash}")
+                    print(f"Process incoming Block: {block_hash}")
+                    receive_hash = await self.nano_to.receive_block(self.private_key, amount, block_hash)
+                    print(f"Receive Block created: {receive_hash}")
+
+                    print(f"Returning funds to account: {source_account}")
+                    refund_hash = await self.nano_to.send(self.private_key, amount, source_account)
+                    print(f"Send Block created : {refund_hash}")
 
             await asyncio.sleep(10)
 
